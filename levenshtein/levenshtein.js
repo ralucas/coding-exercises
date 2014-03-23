d3ed26c7bde9d8c5171171642fd53d09550d3d74
R.A. Lucas <richard@richardalucas.com>
2014-03-23 16:55:58 -0400

    refactored levenshtein

--- levenshtein/levenshtein.js
+++ levenshtein/levenshtein.js
@@ -1,40 +1,40 @@
-/**
-CHALLENGE DESCRIPTION:
+var fs  = require("fs"),
+    input = [],
+    words = [],
+    wordArr= [],
+    len,
+    counter = 0;
 
-Two words are friends if they have a Levenshtein distance of 1 (For details see Levenshtein distance).
-That is, you can add, remove, or substitute exactly one letter in word X to create word Y.
-A word’s social network consists of all of its friends, plus all of their friends, and all of their friends’ friends, and so on.
-Write a program to tell us how big the social network for the given word is, using our word list.
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        input.push(line);
+        if (line === 'END OF INPUT') {
+            wordArr = input;
+            wordArr.pop();
+            len = wordArr.length;
+        }
+        words = wordArr.slice(0,len);
 
-INPUT SAMPLE:
+        words.forEach(function(word) {
+            var len = word.length,
+                testWord = '',
+                str = word.split('');
 
-The first argument will be a path to a filename, containing words, and the word list to search in. 
-The first N lines of the file will contain test cases, they will be terminated by string 'END OF INPUT'.
-After that there will be a list of words one per line. 
-E.g
+            for (var i = 0; i < len; i += 1) {
+                var letter = str[i];
 
-recursiveness
-elastic
-macrographies
-END OF INPUT
-aa
-aahed
-aahs
-aalii
-...
-...
-zymoses
-zymosimeters
-OUTPUT SAMPLE:
+                for (var j = 0; j < 26; j += 1) {
+                    var alpha = String.fromCharCode(97 + j);
+                    str[i] = alpha;
+                    testWord = str.join('');
 
-For each test case print out how big the social network for the word is. 
-In sample the social network for the word 'elastic' is 3 and for the word 'recursiveness' is 1. 
-E.g.
-
-1
-3
-1
-Constrains: 
-Number of test cases N in range(15, 30) 
-The word list always will be the same and it's length will be around 10000 words 
-**/
+                    if (testWord === line) {
+                        counter += 1;
+                    }
+                    str[i] = letter;
+                }
+            }
+        });
+    }
+});
+console.log(counter);
